9818627fa88f18ff3132ea6a2d2079794b092168
omosiki <abdullateefsikirat6@gmail.com>
2024-08-06 14:17:53 +0100

    I have been editing and deleting since morning,Dont even know what to do...i watch youTube videos still not geeting it...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import Navbar from './componet/Navbar/Navbar'
-import Footer from './componet/Footer/Footer'
-import Home from './componet/Home/Home' 
-import Main from './componet/Main/Main'
+
 const App = () => {
   return (
     <>
-      <Navbar/>
-      <Home/>
-      {/* <Main/>
-     x
-      <Footer/> */}
+     
+    
+     <Navbar/>
+     
 
     </>
   )
--- src/componet/Navbar/Navbar.jsx
+++ src/componet/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import "./navbar.css"
-import { MdOutlineTravelExplore } from 'react-icons/md';
+// import { MdOutlineTravelExplore } from 'react-icons/md';
 import { IoMdCloseCircle } from "react-icons/io";
 import { TbGridDots } from "react-icons/tb";
 import logo from "./../images/airplane.jpg"
-
+import {Link} from "react-router-dom"
 
 
 
@@ -22,12 +22,16 @@ const Navbar = () => {
 
 
   return (
+    <>
     <section className='navBarSection'>
         <header className="header flex">
             <div className="logoDiv">
               <div className='logo flex'>
+              
              <img src={logo} alt="" />
+             <h1 > Zikrah</h1>
               </div>
+              
             </div>
             <div className={active}>
                 <ul className="navLists flex">
@@ -38,7 +42,7 @@ const Navbar = () => {
                     <a href="" className='navLink'>Packages</a>
                     </li>
                     <li className='navItem'>
-                    <a href="" className='navLink'>Shop</a>
+                    <a href="" className='navLink'>Main</a>
                     </li>
                     <li className='navItem'>
                     <a href="" className='navLink'>About</a>
@@ -47,7 +51,7 @@ const Navbar = () => {
                     <a href="" className='navLink'>Pages</a>
                     </li>
                     {/* integrate it somewhere  */}
-                    <button className='btn'><a href="">Book now</a></button>
+                    {/* <button className='btn'><a href="">Book now</a></button> */}
                 </ul>
                 <div onClick={removeNav} className="closeNavbar">
                 <IoMdCloseCircle className="icon" />
@@ -59,6 +63,52 @@ const Navbar = () => {
             </div>
         </header>
     </section>
+
+
+    <section className="home">
+      <div className="secContainer container">
+      <div className="homeText">
+
+        <h1>Plan Your Trip With Zikrah Travel.....</h1>
+
+         <p className='subTitle'>
+          Travel to your favourite city with respectful of the environment 
+        </p> 
+        <button className='btn'>Explore</button>
+      </div>
+
+
+      <div className="homeCard grid">
+
+        <div className="destination">
+          <label htmlFor="destination"> Destination:</label>
+            <input type="text" placeholder='location....' />
+            {/* <LiaSearchLocationSolid /> */}
+        </div>
+
+        <div className="priceDiv">
+          <label htmlFor="price">Amount:</label>
+           <input type="text" placeholder='$140-$500' />
+        </div>
+
+
+        <div className="dateInput">
+          <label htmlFor="date">Enter date</label>
+          <input type="date"  />
+        </div>
+
+        <button className='btn'>
+          Search
+        </button>
+      </div>
+      </div>
+
+
+      </section>
+      
+
+
+</>
   )
 }
 
